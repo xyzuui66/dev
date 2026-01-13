6e228b9ca1f27258b8fdc6e765e4d51f0393e5a0
Cezidev <serupaindah665@gmail.com>
2026-01-13 21:41:38 +0700

    Update router.js

--- router.js
+++ router.js
@@ -1,11 +1,12 @@
-const { getFullData, updateFeature, masterCommand } = require('./controller.js');
+const ctrl = require('./controller.js');
 
 exports.handleRequest = (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
     
-    if (url.pathname === '/get-full-data') return getFullData(res);
-    if (url.pathname === '/toggle-feature') return updateFeature(url, res);
-    if (url.pathname === '/master-cmd') return masterCommand(url, res);
+    if (url.pathname === '/get-full-data') return ctrl.getFullData(res);
+    if (url.pathname === '/register') return ctrl.registerNode(url, res);
+    if (url.pathname === '/toggle-feature') return ctrl.updateFeature(url, res);
+    if (url.pathname === '/master-cmd') return ctrl.masterCommand(url, res);
     
-    res.end("404_CORE_NOT_FOUND");
+    res.end("SYSTEM_READY");
 };
